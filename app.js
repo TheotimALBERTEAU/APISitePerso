c673bfe4d3639dccea549f9afe30891154b7f265
Théotim ALBERTEAU <sunshouwu@outlook.com>
2025-12-19 14:26:35 +0100

    Update files

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/skills');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -21,9 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
